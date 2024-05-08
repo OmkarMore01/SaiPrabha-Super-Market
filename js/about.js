4f0aa7a1448d99f1157cea17378111c176121d70
OmkarMore01 <omore1245@gmail.com>
2024-05-08 11:58:36 +0530

    navbar and footer added to index and about page

--- js/about.js
+++ js/about.js
@@ -33,67 +33,93 @@ function instacolor5() {
   instaid.classList.add("empty");
 }
 
-// Adding facebook background
-function emptyfacebook() {
-  const gitid = document.getElementById("facebook");
-  gitid.classList.add("facebook");
+function instacolor6() {
+  const instaid = document.getElementById("instaid6");
+  instaid.classList.add("empty");
 }
 
-function emptyfacebook1() {
-  const gitid = document.getElementById("facebook1");
-  gitid.classList.add("facebook");
+function instacolor7() {
+  const instaid = document.getElementById("instaid7");
+  instaid.classList.add("empty");
 }
 
-function emptyfacebook2() {
-  const gitid = document.getElementById("facebook2");
-  gitid.classList.add("facebook");
+function instacolor8() {
+  const instaid = document.getElementById("instaid8");
+  instaid.classList.add("empty");
 }
-
-function emptyfacebook3() {
-  const gitid = document.getElementById("facebook3");
-  gitid.classList.add("facebook");
-}
-
-function emptyfacebook4() {
-  const gitid = document.getElementById("facebook4");
-  gitid.classList.add("facebook");
-}
-
-function emptyfacebook5() {
-  const gitid = document.getElementById("facebook5");
-  gitid.classList.add("facebook");
-}
-
-// Removing facebook background
+// Removing linked logo background
 function facebookcolor() {
-  const gitid = document.getElementById("facebook");
-  gitid.classList.add("emptyfacebook");
+  const linkedinid = document.getElementById("linkedin");
+  linkedinid.classList.add("emptylinkedin");
+
 }
 
-function facebookcolor1() {
-  const gitid = document.getElementById("facebook1");
-  gitid.classList.add("emptyfacebook");
+function linkedincolor1() {
+  const linkedinid = document.getElementById("linkedin1");
+  linkedinid.classList.add("emptylinkedin");
+
 }
 
-function facebookcolor2() {
-  const gitid = document.getElementById("facebook2");
-  gitid.classList.add("emptyfacebook");
+function linkedincolor2() {
+  const linkedinid = document.getElementById("linkedin2");
+  linkedinid.classList.add("emptylinkedin");
+
 }
 
 function facebookcolor3() {
-  const gitid = document.getElementById("facebook3");
-  gitid.classList.add("emptyfacebook");
+  const linkedinid = document.getElementById("linkedin3");
+  linkedinid.classList.add("emptylinkedin");
+
 }
 
 function facebookcolor4() {
-  const gitid = document.getElementById("facebook4");
-  gitid.classList.add("emptyfacebook");
+  const linkedinid = document.getElementById("linkedin4");
+  linkedinid.classList.add("emptylinkedin");
+
 }
 
 function facebookcolor5() {
-  const gitid = document.getElementById("facebook5");
-  gitid.classList.add("emptyfacebook");
+  const linkedinid = document.getElementById("linkedin5");
+  linkedinid.classList.add("emptylinkedin");
+
 }
+
+function facebookcolor6() {
+  const linkedinid = document.getElementById("linkedin6");
+  linkedinid.classList.add("emptylinkedin");
+
+}
+// Adding linked logo background
+function emptyfacebook() {
+  const linkedinid = document.getElementById("facebook");
+  linkedinid.classList.add("linkedin");
+}
+
+function emptyfacebook1() {
+  const linkedinid = document.getElementById("facebook1");
+  linkedinid.classList.add("linkedin");
+}
+
+function emptyfacebook2() {
+  const linkedinid = document.getElementById("facebook2");
+  linkedinid.classList.add("linkedin");
+}
+
+function emptyfacebook3() {
+  const linkedinid = document.getElementById("facebook3");
+  linkedinid.classList.add("linkedin");
+}
+
+function emptyfacebook4() {
+  const linkedinid = document.getElementById("facebook4");
+  linkedinid.classList.add("linkedin");
+}
+
+function emptyfacebook5() {
+  const linkedinid = document.getElementById("facebook5");
+  linkedinid.classList.add("linkedin");
+}
+
 
 //Removing logo background after mouse down
 function empty() {
@@ -128,100 +154,160 @@ function empty5() {
   instaid.classList.add("insta");
 }
 
+function empty6() {
+  const instaid = document.getElementById("instaid6");
+  instaid.classList.add("insta");
+}
+
+function empty7() {
+  const instaid = document.getElementById("instaid7");
+  instaid.classList.add("insta");
+}
+
+function empty8() {
+  const instaid = document.getElementById("instaid8");
+  instaid.classList.add("insta");
+}
+
 //image javascript change() function
 
-function dblchange() {
+function change() {
   const img = document.getElementById("img");
   img.src = "./../img/about/om.jpg";
 
   const para = document.getElementById("para");
-  para.innerHTML = "Owner";
+  para.innerHTML = "Hii I Am Full Stack Developer";
 }
 
 
-function dblchange1() {
+function change1() {
   const img = document.getElementById("img1");
   img.src = "./../img/about/bhakti1.jpg";
 
   const para = document.getElementById("para1");
-  para.innerHTML = "Manager";
+  para.innerHTML = "Hii I Am Web-Designer";
 }
 
-function dblchange2() {
-  const img = document.getElementById("img2");
-  img.src = "./../img/about/atharva.jpg";
-
-  const para = document.getElementById("para2");
-  para.innerHTML = "Bill Creator";
-}
-
-function dblchange3() {
-  const img = document.getElementById("img3");
-  img.src = "./../img/about/sagar1.jpg";
-
-  const para = document.getElementById("para3");
-  para.innerHTML = "Salesmen";
-}
-
-function dblchange4() {
-  const img = document.getElementById("img4");
-  img.src = "./../img/about/sanika1.jpg";
-
-  const para = document.getElementById("para4");
-  para.innerHTML = "Saleswomen";
-}
-
-function dblchange5() {
-  const img = document.getElementById("img5");
-  img.src = "./../img/about/akanksha1.jpg";
-
-  const para = document.getElementById("para5");
-  para.innerHTML = "Salesmen";
-}
-
-
-//to Regain Original Information
-function change() {
-  const img = document.getElementById("img");
-  img.src = "./../img/about/omkar.jpg";
-
-  const para = document.getElementById("para");
-  para.innerHTML = "Hii I am the Owner";
-}
-
-function change1() {
-  const img = document.getElementById("img1");
-  img.src = "./../img/about/bhakti.jpg";
-
-  const para = document.getElementById("para1");
-  para.innerHTML = "Hi i am Manager";
-}
 function change2() {
   const img = document.getElementById("img2");
   img.src = "./../img/about/atharva.jpg";
 
   const para = document.getElementById("para2");
-  para.innerHTML = "Hi i am  Bill Creator";
+  para.innerHTML = "Hii I Am UI/UX Designer";
 }
+
 function change3() {
+  const img = document.getElementById("img3");
+  img.src = "./../img/about/sagar1.jpg";
+
+  const para = document.getElementById("para3");
+  para.innerHTML = "Hii I Am Shopify Developer";
+}
+
+function change4() {
+  const img = document.getElementById("img4");
+  img.src = "./../img/about/sanika1.jpg";
+
+  const para = document.getElementById("para4");
+  para.innerHTML = "Hii I Am Web Content Manager";
+}
+
+function change5() {
+  const img = document.getElementById("img5");
+  img.src = "./../img/about/akanksha1.jpg";
+
+  const para = document.getElementById("para5");
+  para.innerHTML = "Hii I Am Frontend Developer";
+}
+
+function change6() {
+  const img = document.getElementById("img6");
+  img.src = "./../img/about/harshal.jpg";
+
+  const para = document.getElementById("para6");
+  para.innerHTML = "Hii I Am Web Engineer";
+}
+
+function change7() {
+  const img = document.getElementById("img7");
+  img.src = "./../img/about/sharmin1.jpg";
+
+  const para = document.getElementById("para7");
+  para.innerHTML = "Hii I Am E-commerce Developer";
+}
+
+function change8() {
+  const img = document.getElementById("img8");
+  img.src = "./../img/about/vaishnavi1.jpg";
+
+  const para = document.getElementById("para8");
+  para.innerHTML = "Hii I Am JavaScript Developer";
+}
+//to Regain Original Information
+function dblchange() {
+  const img = document.getElementById("img");
+  img.src = "./../img/about/omkar.jpg";
+
+  const para = document.getElementById("para");
+  para.innerHTML = "Designer";
+}
+
+function dblchange1() {
+  const img = document.getElementById("img1");
+  img.src = "./../img/about/bhakti.jpg";
+
+  const para = document.getElementById("para1");
+  para.innerHTML = "Designer";
+}
+function dblchange2() {
+  const img = document.getElementById("img2");
+  img.src = "./../img/about/atharva.jpg";
+
+  const para = document.getElementById("para2");
+  para.innerHTML = "Designer";
+}
+function dblchange3() {
   const img = document.getElementById("img3");
   img.src = "./../img/about/sagar.jpg";
 
   const para = document.getElementById("para3");
-  para.innerHTML = " Hi i am Customer Service Representative...";
+  para.innerHTML = "Designer";
 }
-function change4() {
+function dblchange4() {
   const img = document.getElementById("img4");
   img.src = "./../img/about/sanika.jpg";
 
   const para = document.getElementById("para4");
-  para.innerHTML = " Hi i am stock manager...";
+  para.innerHTML = "Designer";
 }
-function change5() {
+function dblchange5() {
   const img = document.getElementById("img5");
   img.src = "./../img/about/akanksha.jpg";
 
   const para = document.getElementById("para5");
-  para.innerHTML = " Hi i am cashier...";
+  para.innerHTML = "Designer";
 }
+function dblchange6() {
+  const img = document.getElementById("img6");
+  img.src = "./../img/about/harshal1.jpg";
+
+  const para = document.getElementById("para6");
+  para.innerHTML = "Designer";
+}
+function dblchange7() {
+  const img = document.getElementById("img7");
+  img.src = "./../img/about/sharmin.jpg";
+
+  const para = document.getElementById("para7");
+  para.innerHTML = "Designer";
+}
+function dblchange8() {
+  const img = document.getElementById("img8");
+  img.src = "./../img/about/vaishnavi.jpg";
+
+  const para = document.getElementById("para8");
+  para.innerHTML = "Designer";
+}
+
+
 
