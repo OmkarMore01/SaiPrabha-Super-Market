89bf79620b9c11ef73befc8359deadffe6e092f6
OmkarMore01 <omore1245@gmail.com>
2024-05-13 12:45:21 +0530

    login and register page added

---
+++ js/regi.js
@@ -0,0 +1,54 @@
+ function update()
+
+ {
+    const uname=document.getElementById('uname').value;
+    const pass=document.getElementById('pass').value;
+    const phone=document.getElementById('phone').value;
+    const Address=document.getElementById('address').value;
+    const email=document.getElementById('email').value;
+    const anchor=document.getElementById('regi-anchor');
+
+         if(pass && uname)
+         {
+            
+           
+            if(phone && Address && email)
+                {   
+                    validate(uname,pass);
+                    console.log(`value inserted succesfully`);
+                }
+            else
+            {
+                window.alert(`Enter phone ,address and email`);
+                anchor.href='./../pages/register.html';
+            }
+           
+         }
+         
+         else
+        {
+            window.alert(`Enter username and password`);
+            anchor.href='./../pages/register.html';
+        }
+        return 1;
+}
+function validate(un,ps)
+{
+    
+    const uname=un;
+    localStorage.setItem("username", uname);
+    const pass=ps;
+    localStorage.setItem("password",pass);
+    const name=localStorage.getItem("username");
+    if(name)
+        {
+            window.alert(` Registered Succesfully`);
+            
+        }
+        
+    else
+    {
+        window.alert(`!Registration is not succesfull`);
+    }
+    
+}
