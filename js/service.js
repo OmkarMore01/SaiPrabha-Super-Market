08b20d02b261c5ab22375d020f7609b0115780e1
OmkarMore01 <omore1245@gmail.com>
2024-05-19 09:56:15 +0530

    All work is done

--- js/service.js
+++ js/service.js
@@ -1,6 +1,10 @@
 function change()
 {
-    document.getElementById(big-discount);
-    document.getElementById();
-    document.getElementById();
+ const bigdiscount= document.getElementById('bigdiscount');
+ const offerinfo=  document.getElementById('offer-info');
+ const offerimg=  document.getElementById('info-img');
+    bigdiscount.innerText="Maha Sale";
+    offerinfo.innerText="Peanuts: Nutritious and crunchy peanuts, perfect for snacks and recipes. Buy one pack, get one 50% off! .......Delicious Discounts on Your Favorite Dairy Products!Milk: Fresh, whole, and skim milk options. Buy two gallons, get the third free! Yogurt: Creamy and rich yogurts in various flavors. Buy three, get one free!";
+    offerimg.src="./../img/service/fruitdiscount.jpeg";
+
 }
